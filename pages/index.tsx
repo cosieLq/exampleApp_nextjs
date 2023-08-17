71f2b865298e2bb1ed4c06ecdb067763c08d82fa
cosieLq <32099671+cosieLq@users.noreply.github.com>
2023-08-17 10:16:01 +0200

    Set up example app

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,11 @@
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
-  return null
+  return (
+    <div className="notification">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+    </div>
+  );
 }
